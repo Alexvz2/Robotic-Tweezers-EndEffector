cb0ba0d6ecc0b02dd3a6174dc2fd775de6049362
Alexvz2 <alexvzvz28@gmail.com>
2022-02-11 11:43:40 -0800

    2x scale working, now gotta clean

--- MC/ESPTest/src/main.cpp
+++ MC/ESPTest/src/main.cpp
@@ -1,100 +1,62 @@
 #include <Arduino.h>
-#include "HX711.h"
+#include "HX711-multi.h"
 
-#define enB D8
-#define in3 D6
-#define in4 D7
+// Pins to the load cell amp
+#define CLK D0      // clock pin to the load cell amp
+#define DOUT1 D1    // data pin to the first lca
+#define DOUT2 D2    // data pin to the second lca
 
-#define DOUT  D1
-#define CLK  D2
+#define BOOT_MESSAGE "MIT_ML_SCALE V0.8"
 
-#define button D3
-int pressed = false;
- 
-HX711 scale;
+#define TARE_TIMEOUT_SECONDS 4
 
-//Change this calibration factor as per your load cell once it is found you many need to vary it in thousands
-long calibration_factor = -115741; //-106600 worked for my 40Kg max scale setup 
- 
-//=============================================================================================
-//                         SETUP
-//=============================================================================================
+byte DOUTS[2] = {DOUT1, DOUT2};
+
+#define CHANNEL_COUNT sizeof(DOUTS)/sizeof(byte)
+
+long int results[CHANNEL_COUNT];
+
+HX711MULTI scales(CHANNEL_COUNT, DOUTS, CLK);
+
+void tare();
+
 void setup() {
   Serial.begin(115200);
-  scale.begin(DOUT, CLK);
-  long calibration_factor = scale.read_average(); //Get a baseline reading
-  Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
-  Serial.println(calibration_factor);
-  // Pin Modes
-  pinMode(enB, OUTPUT);
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
-  pinMode(button, INPUT);
-
-  // Set initial rotation direction
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH);
-
-}
- 
-//=============================================================================================
-//                         LOOP
-//=============================================================================================
-void loop() {
-  analogWrite(enB, 96); // Send PWM signal to L298N Enable pin, 95 is the slowest possible 
-  // Read button - Debounce
-  if (digitalRead(button) == true) {
-    digitalWrite(in3, HIGH);
-    digitalWrite(in4, LOW);
-  } else  {
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, HIGH);
-  }
-  if (scale.is_ready()) {
-    long reading = abs(scale.read())+calibration_factor;
-    Serial.print("HX711 reading: ");
-    Serial.println(reading);
-  } else {
-    Serial.println("HX711 not found.");
-  }
-
-  delay(1000);
+  Serial.println(BOOT_MESSAGE);
+  pinMode(CLK,OUTPUT);
   
+  tare();
 }
 
 
-// int pwmOutput = 0 ;
+void tare() {
+  bool tareSuccessful = false;
 
-// void setup() {
-//   Serial.begin(115200);
-//   pinMode(enA, OUTPUT);
-//   pinMode(in1, OUTPUT);
-//   pinMode(in2, OUTPUT);
-//   // Set initial rotation direction
-//   digitalWrite(in1, LOW);
-//   digitalWrite(in2, HIGH);
-// }
+  unsigned long tareStartTime = millis();
+  while (!tareSuccessful && millis()<(tareStartTime+TARE_TIMEOUT_SECONDS*1000)) {
+    tareSuccessful = scales.tare(20,10000);  //reject 'tare' if still ringing
+  }
+}
 
-// void loop() {
-//   digitalWrite(LED, HIGH);
-//   Serial.println("LED is ON!");
-//   delay(1000);
-//   pinMode(LED, OUTPUT);
-//   if (Serial.available() > 0) {
-//     // read the incoming byte:
-//     pwmOutput = Serial.read();
+void sendRawData() {
+  scales.read(results);
+  for (int i=0; i<scales.get_count(); ++i) {;
+    Serial.print( -results[i]);  
+    Serial.print( (i!=scales.get_count()-1)?"\t":"\n");
+  }  
+  delay(10);
+}
 
-//     // say what you got:
-//     Serial.print("I received: ");
-//     Serial.println(pwmOutput, DEC);
-//     // If -1 switch direction
-//     if (pwmOutput == -1) {
-//       digitalWrite(in1, HIGH);
-//       digitalWrite(in2, LOW);
-//     } else {
-//       analogWrite(enA, pwmOutput); // Send PWM signal to L298N Enable pin
-//       digitalWrite(in1, LOW);
-//       digitalWrite(in2, HIGH);
-//     }
-//   }
-// }
+void loop() {
+  
+  sendRawData(); //this is for sending raw data, for where everything else is done in processing
+
+  //on serial data (any data) re-tare
+  if (Serial.available()>0) {
+    while (Serial.available()) {
+      Serial.read();
+    }
+    tare();
+  }
+ 
+}
