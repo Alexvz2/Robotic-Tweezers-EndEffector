dcf85bf48a6eee400e1f353e240726177b7beb5c
Alexvz2 <alexvzvz28@gmail.com>
2022-02-12 15:51:09 -0800

    Open and close claw without deformation, need to test and tune parameters

--- MC/ESPTest/src/main.cpp
+++ MC/ESPTest/src/main.cpp
@@ -14,12 +14,18 @@ int pressed = false;
 // Hall Sensors
 #define HallA D5
 #define HallB D2
+#define RPM_RATIO 125
+#define PPR 245 // pulses per revolution
+#define ShaftCircumference 22 // 2*pi*r in mm
+#define MaxDistance 100 //MM
 
 // ISR
 volatile int pulses = 0;//if the interrupt will change this value, it must be volatile
 bool lock = false;
 
 void IRAM_ATTR interruptA();
+int motorLocation(int);
+void motorRotation();
 
 //=============================================================================================
 //                         SETUP
@@ -44,23 +50,30 @@ void setup() {
 //                         LOOP
 //=============================================================================================
 void loop() {
-  if (lock)
-  {
-    Serial.println(pulses);//see the counts advance
-    lock = false;
-  }
-  // Motor off if button is pressed
-  if (digitalRead(button) == false) {
-    digitalWrite(in3, HIGH);
-    digitalWrite(in4, LOW);
-  } else  {
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, HIGH);
-  }
+  motorRotation();
   delay(100);
 }
 
+void motorRotation(){
+  if (lock){
+    // set motor rotation based on distance
+    if (motorLocation(pulses) < MaxDistance) { // true mean cw
+      digitalWrite(in3, HIGH);
+      digitalWrite(in4, LOW);
+    } else  {
+      digitalWrite(in3, LOW); // false mean ccw
+      digitalWrite(in4, HIGH);
+    }
+  }
+}
+
+// Return displacement of claw based on pulses
+int motorLocation(int pulses){
+  return pulses*ShaftCircumference/PPR;
+}
+
 void IRAM_ATTR interruptA(){
+  lock = false;
   pulses++;
   lock = true;
 }//end Interrupt Service Routine (ISR)
