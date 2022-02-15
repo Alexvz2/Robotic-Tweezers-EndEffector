4ebde209e010df704ca5386286893b80c3e3b091
Alexvz2 <alexvzvz28@gmail.com>
2022-02-15 14:03:44 -0800

    added multi-hx711

--- MC/ESPTest/src/main.cpp
+++ MC/ESPTest/src/main.cpp
@@ -1,5 +1,21 @@
 #include <Arduino.h>
+#include "HX711-multi.h"
 
+// Pins to the load cell amp
+#define CLK D0      // clock pin to the load cell amp
+#define DOUT1 D1    // data pin to the first lca
+#define DOUT2 D2    // data pin to the second lca
+
+// Load Cell Definitions
+#define TARE_TIMEOUT_SECONDS 4
+
+byte DOUTS[2] = {DOUT1, DOUT2};
+
+#define CHANNEL_COUNT sizeof(DOUTS)/sizeof(byte)
+
+long int results[CHANNEL_COUNT];
+
+HX711MULTI scales(CHANNEL_COUNT, DOUTS, CLK);
 
 // Motor
 #define enB D8
@@ -22,14 +38,21 @@ int direction;
 volatile int pulses = 0;//if the interrupt will change this value, it must be volatile
 bool lock = false;
 
+//=============================================================================================
+//                         FNC Definition
+//=============================================================================================
+
 void IRAM_ATTR interruptA();
 void motorRotation(bool);
+void sendRawData();
 
 //=============================================================================================
 //                         SETUP
 //=============================================================================================
 void setup() {
   // Pin Modes
+  pinMode(CLK,OUTPUT);
+  scales.set_gain(32);
   pinMode(enB, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
@@ -43,26 +66,30 @@ void setup() {
   digitalWrite(in4, HIGH);
 
   direction = digitalRead(Dir);
-
+  Serial.print("RPMs:\t");
+  Serial.print("Pulses:\t");
   Serial.begin(115200);
-  analogWrite(enB, 50);
 }
  
 //=============================================================================================
 //                         LOOP
 //=============================================================================================
 void loop() {
-  value = map(analogRead(PWM), 0, 1023, 30, 100);
-  Serial.println(value);
-  if (lock)
+  Serial.println();
+  value = map(analogRead(PWM), 0, 1023, 35, 85); //35 is cut off, -> 45
+  analogWrite(enB, value);
+  Serial.print(value);
+  Serial.print("\t");
+  Serial.print(pulses);
+
+  // Load Cells
+  if (scales.is_ready())
   {
-    Serial.println(pulses);//see the counts advance
-    lock = false;
-  }
+    sendRawData();}
 
   // Direction Logic
   direction = digitalRead(Dir);
-  if (direction != prevDir)
+  if (direction != prevDir) // conditional to only call function on change
   {
     prevDir = direction;
     motorRotation(direction);
@@ -82,7 +109,20 @@ void motorRotation(bool rotation){
   }
 }
 
+void sendRawData() {
+  scales.read(results);
+  for (int i=0; i<scales.get_count(); ++i) {;
+    Serial.print("\t");
+    Serial.print( -results[i]);  
+    Serial.print( (i!=scales.get_count()-1)?"\t":"\n");
+  }  
+  delay(10);
+}
+
 void IRAM_ATTR interruptA(){
-  pulses++;
-  lock = true;
+  if (direction){
+    pulses++;
+  } else {
+    pulses--;
+  }
 }//end Interrupt Service Routine (ISR)
