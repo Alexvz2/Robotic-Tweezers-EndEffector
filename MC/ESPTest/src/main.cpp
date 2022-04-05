5601459136ceec69b4846cb7f1605c5058853a85
rzokol <64999173+rzokol@users.noreply.github.com>
2022-04-04 17:45:28 -0700

    Some big changes

--- MC/ESPTest/src/main.cpp
+++ MC/ESPTest/src/main.cpp
@@ -1,26 +1,70 @@
 #include <Arduino.h>
-
+#include "ESP32_S2_TimerInterrupt.h"
 
 #include <stdlib.h>
 #include <string.h>
 
-
+#define PWMPin1 3
+#define PWMPin2 2
+#define hallA 5
+#define hallB 4
+#define inputPin 1
 float v;
 int tick = 0;
 float pos = 0;
-int PWMPin1 = 7;
-int PWMPin2 = 8;
-int hallA = 33;
-int hallB = 34;
-int inputPin = 38;
+
+int test = 0;
+
+
+
 float duty, des, w, posOld;
 bool forw;
 
 int lastHall = 2;
 float vel, error, volt;
 float testCurrent = 0;
+int sense1, sense2, senseOld1, senseOld2;
+
+ESP32Timer ITimer0(0);
+void setupPins();
+void calibrateMotor();
+void interruptA();
+void interruptB();
 
 
+
+
+bool IRAM_ATTR PWMLoop(void* timerNo) {
+
+    error = pos - des;
+
+    if (abs(error) < 1.0 / 60)
+        error = 0;
+
+    w = w + error * 0.001;
+
+    volt = -5.3183 * pos - 55.6771 * w;
+    duty = volt * 100 / 5;
+
+    if (duty > 0) {
+        analogWrite(PWMPin2, duty * 1024 / 100);
+        analogWrite(PWMPin1, 0);
+    }
+    else {
+        analogWrite(PWMPin1, -duty * 1024 / 100);
+        analogWrite(PWMPin2, 0);
+    }
+    test = test + 1;
+    return true;
+}
+
+void setupTimer() {
+
+
+
+    ITimer0.attachInterruptInterval(1000, PWMLoop);
+
+}
 
 
 void setup() {
@@ -31,8 +75,11 @@ void setup() {
 
     calibrateMotor();
 
-    attachInterrupt(digitalPinToInterrupt(hallA), interruptA, CHANGE);
-    attachInterrupt(digitalPinToInterrupt(hallB), interruptB, CHANGE);
+    senseOld1 = digitalRead(hallA);
+    senseOld2 = digitalRead(hallB);
+
+    attachInterrupt(digitalPinToInterrupt(hallA), hallSense, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(hallB), hallSense, CHANGE);
     delay(2000);
 
 
@@ -51,7 +98,47 @@ void setup() {
 }
 
 
+void hallSense() {
 
+
+    noInterrupts();
+
+    sense1 = digitalRead(hallA);
+    sense2 = digitalRead(hallB);
+
+    if (senseOld1 && senseOld2) {
+        if (sense1 && !sense2)
+            tick--;
+        if (!sense1 && sense2)
+            tick++;
+    }
+    else if (senseOld1 && !senseOld2) {
+        if (!sense1 && !sense2)
+            tick--;
+        if (sense1 && sense2)
+            tick++;
+    }
+    else if (!senseOld1 && !senseOld2) {
+        if (!sense1 && sense2)
+            tick--;
+        if (sense1 && !sense2)
+            tick++;
+    }
+    else if (!senseOld1 && senseOld2) {
+        if (sense1 && sense2)
+            tick--;
+        if (!sense1 && !sense2)
+            tick++;
+    }
+
+    pos = tick / 65.25 / 2;
+
+    senseOld1 = sense1;
+    senseOld2 = sense2;
+
+    interrupts();
+
+}
 
 
 void interruptA() {
@@ -75,8 +162,6 @@ void interruptA() {
     pos = tick / 65.25 / 2;
 
     interrupts();
-
-
 
 }
 
@@ -103,38 +188,7 @@ void interruptB() {
 
 }
 
-void setupTimer() {
 
-
-
-    timer = timerBegin(0, 80, true);
-    timerAttachInterrupt(timer, &PWMLoop, true);
-    timerAlarmWrite(timer, 1000, true);
-    timerAlarmEnable(timer);
-
-}
-
-void IRAM_ATTR PWMLoop(void) {
-
-    error = pos - des;
-
-    if (abs(error) < 1.0 / 60)
-        error = 0;
-
-    w = w + error * 0.001;
-
-    volt = -5.3183 * pos - 55.6771 * w;
-    duty = volt * 100 / 5;
-
-    if (duty > 0) {
-        analogWrite(PWMPin2, duty * 1024 / 100);
-        analogWrite(PWMPin1, 0);
-    }
-    else {
-        analogWrite(PWMPin1, -duty * 1024 / 100);
-        analogWrite(PWMPin2, 0);
-    }
-}
 
 
 
@@ -143,27 +197,21 @@ void loop() {
     // put your main code here, to run repeatedly:
     float x;
 
-    Serial.println(3.3 / 1024 * analogRead(inputPin));
+
     Serial.print("pos: ");
     Serial.println(pos);
     Serial.print("des: ");
     Serial.println(des);
 
-    x = averageReading(inputPin) * 0.454543632 * 3.3 / 1024;
-    if (x - des > 0.01) {
-        des = des + 0.01;
-    }
-    else if (x - des < -0.01) {
-        des = des - 0.01;
-    }
-    else {
-        des = x;
-    }
+    x = averageReading(inputPin) * 0.393938 * 3.3 / 8192;
+
+    des = x;
+
 
 
     des = x;        // des is 1.5 at 3.3v reading
 
-    delay(10);
+    delay(1);
 
     if (pos < -0.1 || pos>2.1) {
         while (1) {
@@ -172,13 +220,15 @@ void loop() {
             analogWrite(PWMPin2, 0);
         }
     }
+    Serial.println(test);
+    test = 0;
 }
 
 void calibrateMotor() {
     analogWrite(PWMPin1, 0);
-    analogWrite(PWMPin2, 1024 * 0.06);
+    analogWrite(PWMPin2, 1024 * 0.04);
     delay(200);
-    analogWrite(PWMPin1, 1024 * 0.06);
+    analogWrite(PWMPin1, 1024 * 0.04);
     analogWrite(PWMPin2, 0);
     delay(1000);
     analogWrite(PWMPin1, 0);
@@ -197,11 +247,9 @@ void calibrateMotor() {
 void setupPins() {
     pinMode(PWMPin1, OUTPUT);   // Setting PWM pins as outputs
     pinMode(PWMPin2, OUTPUT);
-    pinMode(35, OUTPUT);
     pinMode(hallA, INPUT_PULLUP);
     pinMode(hallB, INPUT_PULLUP);
     pinMode(inputPin, INPUT);
-    digitalWrite(35, 1);
 
 }
 
